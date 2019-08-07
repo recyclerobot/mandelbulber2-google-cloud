66ed9d5bd1fec6fe24290991d7e88728eeb2c59b
Thijs Bernolet <thijs@recyclerobot.com>
2019-08-07 23:02:47 +0200

    update

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ set -e
 # Set some vars
 INSTANCE_NAME=render-instance
 FRACTAL_FILE=./testfile.txt
+GOOGLE_SERVICE_ACCOUNT_KEY=~/Dropbox/Thijs/_BASH/gke.json
+BUCKET_NAME=mandelbulber
 
 # Remove any previous instance with this name
 echo Y | gcloud compute instances delete ${INSTANCE_NAME}
@@ -17,13 +19,22 @@ gcloud compute instances create ${INSTANCE_NAME} \
   --preemptible --machine-type=f1-micro
 
 # Send local file to box
-gcloud compute scp ${FRACTAL_FILE} ${INSTANCE_NAME}:~ --force-key-file-overwrite
+gcloud compute scp ${FRACTAL_FILE} ${GOOGLE_SERVICE_ACCOUNT_KEY} ${INSTANCE_NAME}:~ --force-key-file-overwrite
 
 # SSH Into the box 
-# - install all dependencies
 # - mount bucket as disk
+# - install all dependencies
 # - start rendering
 gcloud compute ssh ${INSTANCE_NAME} --force-key-file-overwrite << EOF
   ls
-  echo 'hello world'
+  export GOOGLE_APPLICATION_CREDENTIALS="~/gke.json"
+  export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+  echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+  sudo apt-get update
+  sudo apt-get install gcsfuse
+  sudo mkdir /disk
+  echo ${BUCKET_NAME}
+  gcsfuse ${BUCKET_NAME} /disk
+  ls /disk
 EOF
