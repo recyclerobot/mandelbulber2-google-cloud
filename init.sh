21072a04334f27a6e8e212c81602a3fcc1f9c624
Thijs Bernolet <thijs@recyclerobot.com>
2019-08-08 00:21:02 +0200

    updates

--- init.sh
+++ init.sh
@@ -2,20 +2,22 @@
 set -e
 
 # Set some vars
-INSTANCE_NAME=render-instance
+INSTANCE_BASE=render-instance
+INSTANCE_NAME=${INSTANCE_BASE}-$(date +'%F--%H%M')
 FRACTAL_FILE=./testfile.txt
 GOOGLE_SERVICE_ACCOUNT_KEY=~/Dropbox/Thijs/_BASH/gke.json
 BUCKET_NAME=mandelbulber
 
 # Remove any previous instance with this name
-echo Y | gcloud compute instances delete ${INSTANCE_NAME}
+# echo Y | gcloud compute instances delete -q ${INSTANCE_BASE}-*
+gcloud compute instances list | grep RUNNING | awk '{printf "gcloud compute instances delete -q %s --zone %s\n", $1, $2}' | bash
 
 # Create a new instance
 # Instance will be preemptible: much cheaper but can be stopped at any time and max 24h
 # https://cloud.google.com/compute/docs/instances/preemptible
 # Set machine type to desired size
 gcloud compute instances create ${INSTANCE_NAME} \
-  --image-family=ubuntu-minimal-1904 --image-project=ubuntu-os-cloud \
+  --image-family=ubuntu-minimal-1804-lts --image-project=ubuntu-os-cloud \
   --preemptible --machine-type=f1-micro
 
 # Send local file to box
@@ -25,16 +27,16 @@ gcloud compute scp ${FRACTAL_FILE} ${GOOGLE_SERVICE_ACCOUNT_KEY} ${INSTANCE_NAME
 # - mount bucket as disk
 # - install all dependencies
 # - start rendering
-gcloud compute ssh ${INSTANCE_NAME} --force-key-file-overwrite << EOF
-  ls
-  export GOOGLE_APPLICATION_CREDENTIALS="~/gke.json"
-  export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
-  echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
-  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-  sudo apt-get update
-  sudo apt-get install gcsfuse
-  sudo mkdir /disk
-  echo ${BUCKET_NAME}
-  gcsfuse ${BUCKET_NAME} /disk
-  ls /disk
-EOF
+gcloud compute ssh ${INSTANCE_NAME} --force-key-file-overwrite -- '
+  pwd && \
+  ls -al && \
+  export GOOGLE_APPLICATION_CREDENTIALS="~/gke.json" && \
+  export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s` && \
+  echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list && \
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
+  sudo apt-get update && \
+  sudo apt-get install gcsfuse && \
+  sudo mkdir /disk && \
+  gcsfuse mandelbulber /disk && \
+  ls /disk \
+'
